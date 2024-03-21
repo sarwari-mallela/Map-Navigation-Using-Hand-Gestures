e61393d94de70463923c291c1b75baf855bc7052
11a <11a_git@proton.me>
2024-03-21 15:59:35 +0100

    back to opening the html file without a server

--- main.py
+++ main.py
@@ -1,44 +1,29 @@
 
 import threading
 import keyboard
-import requests
 import mapnavlib
 from mapnavlib.gestures.gesture_detection import gest_dect
 from mapnavlib.mapweb.open_browser import open_html_n_exec
 from shared_utils import SharedState
-
-def check_server_is_running(url):
-    try:
-        response = requests.get(url)
-        return response.status_code == 200
-    except requests.exceptions.ConnectionError:
-        return False
 
 def main():
     shared_state = SharedState()
     print(f"> MapNavlib version: {mapnavlib.__version__}")
 
     # Create threads for gesture detection and opening the browser
-    gesture_thread = threading.Thread(target=gest_dect, args=(shared_state,))
     browser_thread = threading.Thread(target=open_html_n_exec, args=(shared_state,))
+    gesture_thread = threading.Thread(target=gest_dect, args=(shared_state,))
 
     # Start the threads
-    # gesture_thread.start()
     browser_thread.start()
-
-    # Quick check
-    if check_server_is_running('http://localhost:1234'):
-        print(">>> Server is: running")
-        print("Press 'q' to quit")
-    else:
-        print(">>> Server is: not running")
+    gesture_thread.start()
 
     # Set up a non-blocking key listener
     keyboard.add_hotkey('q', lambda: shared_state.set_exit_flag(True))
 
     # Wait for both threads to complete
-    gesture_thread.join()
     browser_thread.join()
+    gesture_thread.join()
 
 if __name__ == "__main__":
     main()
--- mapnavlib/mapweb/open_browser.py
+++ mapnavlib/mapweb/open_browser.py
@@ -1,39 +1,20 @@
 
 import os
-import threading
-import keyboard
 import webbrowser
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import keyboard
 
 def get_full_path(relative_path):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, relative_path)
 
-def start_server(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
-    try:
-        # Change the current working directory to make server work
-        server_address = ('', port)
-        httpd = server_class(server_address, handler_class)
-        print(f"Starting httpd server on port {port}")
-        httpd.serve_forever()
-    except Exception as e:
-        print(f"Failed to start server: {e}")
-
-def open_html_n_exec(shared_state, port):
+def open_html_n_exec(shared_state):
     print("> Opening browser...")
     relative_path = 'mapmove.html'
     full_path = get_full_path(relative_path)
-    directory = os.path.dirname(full_path)
 
-    # Start the server in a separate thread
-    # Server will keep running in daemon mode until the main program exits
-    server_thread = threading.Thread(target=start_server, args=(HTTPServer, lambda *args, directory='.', **kwargs: SimpleHTTPRequestHandler(*args, directory=directory, **kwargs), port,))
-    server_thread.daemon = True
-    server_thread.start()
+    # This will open the HTML file in the default browser
+    webbrowser.open('file://' + full_path)
 
-    # Opening browser with html file
-    url = f"http://localhost:{port}/{os.path.basename(relative_path)}"
-    webbrowser.open_new(url)
-
+    print("Press 'q' to quit...")
     keyboard.wait('q')
     shared_state.set_exit_flag(True)
